e48e9042aeeb7862c2a44cdd56485da15ae7c15b
Debango-07 <debangoroy321@gmail.com>
2023-09-13 22:31:15 +0530

    Practice

---
+++ Practice/function1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+void print(){
+    cout<<"My name is Arun";
+}
+int main(){
+    print();
+    return 0;
+}
---
+++ Practice/function2.cpp
@@ -0,0 +1,22 @@
+// #include<iostream>
+// using namespace std;
+// void name(){
+//     for(int i=0;i<5;i++){
+//         cout<<"Arun"<<endl;
+//     }
+// }
+// int main(){
+//     name();
+//     return 0;
+// }
+#include<iostream>
+using namespace std;
+void name(){
+    for(int i=1;i<=10;i++){
+        cout<<i<<endl;
+    }
+}
+int main(){
+    name();
+    return 0;
+}
